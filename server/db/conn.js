f8d415622fee4ac4c7477f6c8baaba3fe94e8f1c
1204yashika <1204yashikaagrawal@gmail.com>
2023-04-06 10:32:06 +0530

    new site

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,17 +1,76 @@
-var mysql = require('mysql-await');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Gla@2023",
-  database: "gate_db"
-});
-con.on(`error`, (err) => {
-    console.error(`Connection error ${err.code}`);
-  });
-
-async function runquery(q) {
-    let result = await con.awaitQuery(q);
-    return result;
+const { MongoClient } = require("mongodb");
+let url = `mongodb+srv://1204yashikaagrawal:Yashi1204@cluster0.qqgtc8o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+async function run() {
+    await client.connect();
+    var db = await client.db("csprep_db");
+    var questions = await db.collection("questions");
+    var q1 = await questions.find({Correct: 1});
+    var qs = []
+    await q1.forEach(b=>qs.push(b));
+    return qs;
+}
+async function addUser(myobj){
+    await client.connect();
+    var db = await client.db("csprep_db");
+    var questions = await db.collection("users");
+    try{
+      await questions.insertOne(myobj);
+      return "success";
+    }
+    catch(err){
+      return err.message;
+    }
+    
 }
 
-module.exports = { runquery };
+async function checkUser(myobj){
+  await client.connect();
+    var db = await client.db("csprep_db");
+    var questions = await db.collection("users");
+    try{
+      var x = await questions.findOne(myobj);
+      if (x == null) {
+        return {"status":"failed","message":"Invalid username and password combination"}
+      }
+      else {
+        return {"status":"success","name":x["name"]}
+      }
+    }
+    catch(err){
+      return err.message;
+    }
+}
+
+async function sendExamData(eid,rand,email){
+  await client.connect();
+  var db = await client.db("csprep_db");
+  var exams = await db.collection("exams");
+  try{
+    var res = await exams.insertOne({eid:eid,rstr:rand,email:email,starttime:"",endtime:"",answers:{}});
+    return res.insertedId.toString();
+  }
+  catch(err){
+    return err.message;
+  }
+}
+module.exports = {run, addUser, checkUser, sendExamData};
+// run().then(a=>console.log(a))
+
+// var mysql = require('mysql-await');
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Gla@2023",
+//   database: "gate_db"
+// });
+// con.on(`error`, (err) => {
+//     console.error(`Connection error ${err.code}`);
+//   });
+
+// async function runquery(q) {
+//     let result = await con.awaitQuery(q);
+//     return result;
+// }
+
+// module.exports = { runquery };
--- server/index.js
+++ server/index.js
@@ -2,22 +2,109 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const conn = require("./db/conn.js");
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "Gla2023",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (req, res)=>{
-    res.status(200);
-    res.sendFile("/client/Home.html", {'root': './'});
+    if (req.session.username) {
+        res.redirect("/home");
+    }
+    else {
+        res.sendFile("/client/index.html", {'root': './'});
+    }
+    
 });
 
+app.get('/home', (req, res)=>{
+    if (req.session.username) {
+        res.sendFile("/client/home.html", {'root': './'});
+    }
+    else {
+        res.redirect("/");
+    }
+});
 app.get('/data',(req,res)=>{
     res.status(200);
-    conn.runquery("select * from questions").then(results => res.send(results));
+    conn.run().then(results => console.log(results));
+});
+
+app.post("/register",(req,res)=>{
+    let data = req.body;
+    data["questions"]={};
+    data["_id"]=data["email"];
+    delete data["email"];
+    conn.addUser(data).then(function(r){
+        if(r=="success"){
+            res.status(200);
+            res.send(r);
+        }
+        else{
+            res.status(400);
+            res.send(r);
+        }
+    });
 })
 
+app.post("/login",(req,res)=>{
+    let data = req.body;
+    data["_id"]=data["email"];
+    delete data["email"];
+    conn.checkUser(data).then(function(resp){
+        if(resp["status"]=="success"){
+            res.status(200);
+            req.session.username = data["_id"];
+            res.send(resp);
+        }
+        else{
+            res.status(400);
+            res.send(resp);
+        }
+    });
+    
+})
+
+app.get("/logout",(req,res)=>{
+    req.session.destroy();
+    res.redirect("/");
+})
 app.listen(3000, (error) =>{
     if(!error)
         console.log("Server is Successfully Running,and App is listening on port "+ 3000)
     else 
         console.log("Error occurred, server can't start", error);
     }
-)
+);
+
+app.post("/takeexam",(req,res)=>{
+    if (req.session.username) {
+        resp = req.body;
+        var eid = resp["eid"];
+        var rand = "";
+        if(eid=="CUSTOM"){
+            rand = resp["randomstring"];
+            if(rand==null){
+                res.redirect("/");
+            }
+        }
+        conn.sendExamData(eid,rand,req.session.username).then(function(r2){
+            req.session.examid = r2;
+            res.sendFile("/client/exam.html", {'root': './'});
+        });
+    }
+    else {
+        res.redirect("/");
+    }
+})
 
