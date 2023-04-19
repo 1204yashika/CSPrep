c02d2f42fa15acb5f9bc3dc98c7fe4e5007c0465
codingmastr <codingmaster52@gmail.com>
2023-04-19 14:37:45 +0530

    updated

--- client/script.js
+++ client/script.js
@@ -19,32 +19,42 @@
       };
     }
 
-
+var data = {}
     // Exam Script
     
     // Fetch JSON data
     fetch("GATE15P1.json")
       .then(response => response.json())
-      .then(data => {
+      .then(d => { data = d;
+      dataHtml(0)})
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
     // access the properties of the first object
-   // for (let i = 0; i < data.length; i++) {
-    //    questions.push(data[i]);
-     // }
-    const firstQuestion = data[9];
-    document.querySelector("#question").innerHTML = firstQuestion.Question;
-    document.querySelector("#OptionA").innerHTML = firstQuestion.OptionA;
-    document.querySelector("#OptionB").innerHTML = firstQuestion.OptionB;
-    document.querySelector("#OptionC").innerHTML = firstQuestion.OptionC;
-    document.querySelector("#OptionD").innerHTML = firstQuestion.OptionD;
-    if(firstQuestion.ImageName!=""){
+    
+  function dataHtml(i2){
+    var i = parseInt(i2);
+    document.querySelector("#qno").innerHTML = i+1;
+    document.querySelector("#question").innerHTML = data[i].Question;
+    document.querySelector("#OptionA").innerHTML = data[i].OptionA;
+    document.querySelector("#OptionB").innerHTML = data[i].OptionB;
+    document.querySelector("#OptionC").innerHTML = data[i].OptionC;
+    document.querySelector("#OptionD").innerHTML = data[i].OptionD;
+    document.querySelector("#qid").innerHTML = i;
+    if(data[i].ImageName!=""){
         imgs.style.display = "block";
-        str = "Images/"+firstQuestion.ImageName+".png";
+        str = "Images/"+data[i].ImageName+".png";
         document.querySelector("#imgs>img").setAttribute("src",str);
     }
-  })
+    else{
+      imgs.style.display = "none";
+    }
+  }
+  function nxtque() {
+    var n = document.getElementById("qid").innerHTML;
+    dataHtml(parseInt(n)+1);
+  }
   
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+  
 
     
