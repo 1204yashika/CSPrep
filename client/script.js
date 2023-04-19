7b162b34e9b2a10e993799cb879f3925ea366ca7
1204yashika <1204yashikaagrawal@gmail.com>
2023-04-19 22:24:16 +0530

    added button

--- client/script.js
+++ client/script.js
@@ -32,19 +32,6 @@ var data = {}
         console.log('Error fetching data:', error);
       });
     // access the properties of the first object
-<<<<<<< HEAD
-   // for (let i = 0; i < data.length; i++) {
-    //    questions.push(data[i]);
-     // }
-    
-    const firstQuestion = data[9];
-    document.querySelector("#question").innerHTML = firstQuestion.Question;
-    document.querySelector("#OptionA").innerHTML = firstQuestion.OptionA;
-    document.querySelector("#OptionB").innerHTML = firstQuestion.OptionB;
-    document.querySelector("#OptionC").innerHTML = firstQuestion.OptionC;
-    document.querySelector("#OptionD").innerHTML = firstQuestion.OptionD;
-    if(firstQuestion.ImageName!=""){
-=======
     
   function dataHtml(i2){
     var i = parseInt(i2);
@@ -56,39 +43,10 @@ var data = {}
     document.querySelector("#OptionD").innerHTML = data[i].OptionD;
     document.querySelector("#qid").innerHTML = i;
     if(data[i].ImageName!=""){
->>>>>>> b70b116a487904e51e517d04340d32253ae034ea
         imgs.style.display = "block";
         str = "Images/"+data[i].ImageName+".png";
         document.querySelector("#imgs>img").setAttribute("src",str);
     }
-<<<<<<< HEAD
-  })
-  
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-
-
-function qshtml(qid) {
-  const firstQuestion = data[qid];
-  document.querySelector("#question").innerHTML = firstQuestion.Question;
-  document.querySelector("#OptionA").innerHTML = firstQuestion.OptionA;
-  document.querySelector("#OptionB").innerHTML = firstQuestion.OptionB;
-  document.querySelector("#OptionC").innerHTML = firstQuestion.OptionC;
-  document.querySelector("#OptionD").innerHTML = firstQuestion.OptionD;
-  if(firstQuestion.ImageName!=""){
-      imgs.style.display = "block";
-      str = "Images/"+firstQuestion.ImageName+".png";
-      document.querySelector("#imgs>img").setAttribute("src",str);
-  }
-  else{
-    imgs.style.display = "none";
-  }  
-}
-
-
-    
-=======
     else{
       imgs.style.display = "none";
     }
@@ -97,4 +55,3 @@ function qshtml(qid) {
     var n = document.getElementById("qid").innerHTML;
     dataHtml(parseInt(n)+1);
   }
->>>>>>> b70b116a487904e51e517d04340d32253ae034ea
