57f55523c2dff2436978d9e62d632a85a30bf877
codingmastr <codingmaster52@gmail.com>
2023-05-17 11:22:35 +0530

    "Update"

--- client/script.js
+++ client/script.js
@@ -261,7 +261,7 @@ function dataHtml(i2) {
 // Next Button
 function nxtque() {
   var qid = document.querySelector(".question").getAttribute('id');
-  var sel="";
+  var sel = "";
   console.log(qid);
   var opc = document.getElementsByClassName("form-check");
   for (i of opc) {
@@ -275,7 +275,7 @@ function nxtque() {
     }
   }
   $.ajax({
-    url:'submitQuestion?qid='+qid+'&sel='+sel
+    url: 'submitQuestion?qid=' + qid + '&sel=' + sel
   })
   // document.location.href='submitQuestion?qid='+qid+'&sel='+sel;
   var n = document.getElementById("qid").innerHTML;
@@ -290,7 +290,7 @@ function nxtque() {
 // Skip Button
 function skpque() {
   var qid = document.querySelector(".question").getAttribute('id');
-  var sel="";
+  var sel = "";
   console.log(qid);
   var opc = document.getElementsByClassName("form-check");
   for (i of opc) {
@@ -303,7 +303,7 @@ function skpque() {
       console.log(i.getAttribute('value'));
     }
   }
-  document.location.href='submitQuestion?qid='+qid+'&sel='+sel;
+  document.location.href = 'submitQuestion?qid=' + qid + '&sel=' + sel;
   var n = document.getElementById("qid").innerHTML;
   var n2 = parseInt(n) + 1;
 
@@ -337,74 +337,157 @@ function checkOption(e) {
     }
   });
 
-  
-    
+
+
 })(jQuery);
 
 var rdata = {}
-function SubmitPaper(){
+function SubmitPaper() {
   $.ajax({
-      url: "/submitPaper",
-      method: "post",
-      async: false,
-      success: function (d) {
-        rdata = d[0];
-        console.log(d[0])
-      },
-      error: function (x, y) {
-        alert(x.responseText);
-      },
-    });
-    ans = rdata["answers"];
-    att = ans.length;
-    marks = 0;
-    corr = 0;
-    incorr = 0;
-    natt = 0;
-    time = parseInt(rdata["endtime"])-parseInt(rdata["starttime"]);
-    for (i of ans){
-      if(i['sel']==''){
-        att--;
-      }
-      else{
-        qid = i['qid'];
-        for(q of data){
-          if(q["Qid"]==qid){
-            if(parseInt(q["Correct"])==parseInt(i["sel"])){
-              corr++;
-              marks += parseInt(q["Marks"]);
-            }
-            else{
-              incorr++;
-            }
+    url: "/submitPaper",
+    method: "post",
+    async: false,
+    success: function (d) {
+      rdata = d[0];
+      console.log(d[0])
+    },
+    error: function (x, y) {
+      alert(x.responseText);
+    },
+  });
+  ans = rdata["answers"];
+  att = ans.length;
+  marks = 0;
+  corr = 0;
+  incorr = 0;
+  natt = 0;
+  time = parseInt(rdata["endtime"]) - parseInt(rdata["starttime"]);
+  for (i of ans) {
+    if (i['sel'] == '') {
+      att--;
+    }
+    else {
+      qid = i['qid'];
+      for (q of data) {
+        if (q["Qid"] == qid) {
+          if (parseInt(q["Correct"]) == parseInt(i["sel"])) {
+            corr++;
+            marks += parseInt(q["Marks"]);
+          }
+          else {
+            incorr++;
           }
         }
       }
     }
-    natt = 65 - att;
-    console.log(att +" "+natt+ " "+ marks+ " "+ corr + " "+incorr);
-    $.ajax({
-      url: "/setResultData",
-      method: "post",
-      data: {att:att,natt:natt,marks:marks,corr:corr,incorr:incorr,time:time},
-      async: false,
-      success: function(){
-        document.location.href = "Result";
-      },
-      error: function (x, y) {
-        alert(x.responseText);
-      },
-    });
+  }
+  natt = 65 - att;
+  console.log(att + " " + natt + " " + marks + " " + corr + " " + incorr);
+  $.ajax({
+    url: "/setResultData",
+    method: "post",
+    data: { att: att, natt: natt, marks: marks, corr: corr, incorr: incorr, time: time },
+    async: false,
+    success: function () {
+      document.location.href = "Result";
+    },
+    error: function (x, y) {
+      alert(x.responseText);
+    },
+  });
 }
 
-function getData(){
+
+function getData() {
   $.ajax({
     url: "/getResultData",
     method: "get",
     async: false,
-    success: function(d){
+    success: function (d) {
       rdata = d;
       console.log(rdata);
+      const marks = document.getElementById('marks');
+      marks.innerHTML = rdata.marks;
+      const ctx = document.getElementById('myChart').getContext("2d");
+      const marksObtained = rdata.marks;
+      const totalMarks = 100;
+      const percentage = (marksObtained / totalMarks) * 100;
+      new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: ["Marks Obtained", "Remaining"],
+          datasets: [
+            {
+              label: "Percentage",
+              data: [percentage, 100 - percentage],
+              backgroundColor: ["rgba(239, 255, 0, 0.8)", "rgba(255, 56, 56, 0.8)"],
+              borderColor: ["rgba(239, 255, 0, 1)", "rgba(106, 255, 0, 0)"],
+              borderWidth: 2,
+            },
+          ],
+        },
+      });
+
+      // Convert time from milliseconds to hours:minutes:seconds format
+      function formatTime(milliseconds) {
+        let seconds = Math.floor(milliseconds / 1000);
+        let hours = Math.floor(seconds / 3600);
+        seconds %= 3600;
+        let minutes = Math.floor(seconds / 60);
+        seconds %= 60;
+
+        return `${hours}:${padZero(minutes)}:${padZero(seconds)}`;
+      }
+      // Pad zero if the value is less than 10
+      function padZero(value) {
+        return value < 10 ? `0${value}` : value;
+      }
+      const time = document.getElementById('time');
+      const timeInMilliseconds = rdata.time;
+      time.textContent = formatTime(timeInMilliseconds);
+
+      const correct = document.getElementById('correct');
+      correct.innerHTML = rdata.corr;
+      const incorrect = document.getElementById('inCorrect');
+      incorrect.innerHTML = rdata.incorr;
+      const incorrQue = rdata.incorr;
+      const corrQue = rdata.corr;
+      const ctx2 = document.getElementById('myChart2').getContext("2d");
+      new Chart(ctx2, {
+        type: 'pie',
+        data: {
+            labels: ['Correct Questions', 'In-Correct Questions'],
+            datasets: [{
+                label: 'Questions',
+                data: [corrQue,incorrQue],
+                backgroundColor: ["rgba(16, 5, 254, 0.8)", "rgba(255, 0, 244, 0.5)"],
+                borderColor: ["rgba(16, 5, 254, 1)", "rgba(106, 255, 0, 0)"],
+                borderWidth: 2
+            }]
+        },
+    });
+
+
+      const total = 65;
+      const attempted = document.getElementById('attempted');
+      attempted.innerHTML = rdata.att;
+      const attemptedQue = rdata.att;
+      const ctx3 = document.getElementById('myChart3').getContext("2d");
+      new Chart(ctx3, {
+          type: 'pie',
+          data: {
+              labels: ['Attempted Questions', 'Not Attempted Questions'],
+              datasets: [{
+                  label: 'Questions',
+                  data: [attemptedQue, 65 - attemptedQue],
+                  backgroundColor: ["rgba(255, 0, 0, 0.84)", "rgba(106, 255, 0, 0.61)"],
+                  borderColor: ["rgba(255, 0, 0, 1)", "rgba(106, 255, 0, 0)"],
+                  borderWidth: 2
+              }]
+          },
+      });
+      const notattempted = document.getElementById('notattempted');
+      notattempted.innerHTML = rdata.natt;
     },
     error: function (x, y) {
       alert(x.responseText);
